f6fdb150464c5792e7b428857399c541bade5aff
JackWanaCode <kazucuong@yahoo.com>
2018-08-17 20:23:04 +0000

    fork the program

--- arguments.c
+++ arguments.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "shell.h"
 #define BUFSIZE 1024
 
-int main()
+int main(void)
 {
-	int i = 0, j = 0;
-	int status = 1;
+/*	int i = 0, j = 0;
+ */	int status = 1;
 	int read;
 	size_t size = BUFSIZE;
 	char *buffer;
 	char **arguments = NULL;
+	pid_t child_pid;
 
 	while(status)
 	{
@@ -28,14 +31,20 @@ int main()
 		else
 		{
 			arguments = malloc(sizeof(char) * size);
-			j = string_split(buffer, arguments);
-			for (i = 0; i < j; i++)
-				printf("%s\n", arguments[i]);
-			if (execve(arguments[0], arguments, NULL) == -1)
+			string_split(buffer, arguments);
+			child_pid = fork();
+			if (child_pid == -1)
 			{
 				perror("Error:");
+				return (1);
 			}
-			printf("after excution");
+			else if (child_pid == 0)
+			{
+				if (execve(arguments[0], arguments, NULL) == -1)
+					perror("Error:");
+			}
+			else
+				wait(NULL);
 			free(arguments);
 		}
 	}
