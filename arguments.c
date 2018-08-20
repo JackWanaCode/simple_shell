83f537f3b00c3b8de0dd970318071e53182a84b6
Robert <robertglatzel@gmail.com>
2018-08-20 22:03:47 +0000

    Fixed issues on 4.8.4 compiler seg fault

--- arguments.c
+++ arguments.c
@@ -13,8 +13,8 @@ int main(void)
 	int status = 1;
 	int read;
 	size_t size = BUFSIZE;
-	char *buffer;
-	char *f_av;
+	char *buffer = NULL;
+	char *f_av = NULL;
 	char f_av1[BUFSIZE];
 	char f_av2[BUFSIZE];
 	char **arguments = NULL;
@@ -33,7 +33,7 @@ int main(void)
 			status = 0;
 		else
 		{
-			arguments = malloc(sizeof(char) * size);
+			arguments = malloc(sizeof(char) * read);
 			string_split(buffer, arguments, read);
 			if (_strcmp(arguments[0], "cd") == 0)
 			{
@@ -47,7 +47,7 @@ int main(void)
 			_strcpy(f_av1, "/bin/");
 			_strcpy(f_av2, "/usr/bin/");
 			f_av = argv_check(arguments[0], arguments[1], f_av1, f_av2);
-			/* fork the program */
+/* fork the program */
 			child_pid = fork();
 			if (child_pid == -1)
 			{
@@ -57,16 +57,15 @@ int main(void)
 			else if (child_pid == 0)
 			{
 
-				if (execve(f_av, arguments, NULL) != -1)
-					free(arguments);
-				else
-						perror("Error:");
+				if (execve(f_av, arguments, NULL) == -1)
+					perror("execve error");
 			}
 			else
 			{
 				wait(NULL);
 			}
 		}
+		free(arguments);
 	}
 	return (0);
 }
