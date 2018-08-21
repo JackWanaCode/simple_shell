11ee80c4863023c6f6f0d7c50891896f7b4a5cfe
RobertGlatzel <robertglatzel@gmail.com>
2018-08-21 05:05:16 +0000

    Moved all headers and prototypes into shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,13 @@
 #ifndef __SIMPLE_SHELL__
 #define __SIMPLE_SHELL__
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int string_split(char *buffer, char **array, int read);
 char *argv_check(char *av0, char *av1, char *f_av1, char *f_av2);
@@ -7,5 +15,6 @@ char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _stoi(char *s);
+int _putstring(char *str);
 
 #endif
