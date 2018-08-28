42df5eb689ccd2f75057ae02081d1875398cba42
JackWanaCode <kazucuong@yahoo.com>
2018-08-28 18:23:41 +0000

    return prompt when user hit spacebar or tab only

--- arguments.c
+++ arguments.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv, char **env)
 	char *buffer = NULL, prev_cwd[200];
 	int rd = 0, ct = 0;
 	size_t size = 0;
-	int status = 1;
+	int status = 1, i = 0, check;
 	char *name;
 
 	(void)argc;
@@ -16,14 +16,18 @@ int main(int argc, char **argv, char **env)
 	while (status)
 	{
 		ct++;
+		check = 0;
 		_putstring("$ ");
 		rd = getline(&buffer, &size, stdin);
-		if (rd != 1 && rd != -1)
-			exec_func(buffer, rd, name, prev_cwd, ct, env);
-		else if (rd == 1)
+		for (i = 0; buffer[i] != '\0'; i++)
+			if (buffer[i] == ' ' || buffer[i] == '\t')
+				check++;
+		if (rd == 1 || rd == check + 1)
 			continue;
 		else if (rd == -1)
-			status = 0;
+                        status = 0;
+		else
+			exec_func(buffer, rd, name, prev_cwd, ct, env);
 	}
 	if (buffer)
 		free(buffer);
