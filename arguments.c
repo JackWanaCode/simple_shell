62975b43a565cf3dc858a10dddd91efc9305e1df
Robert <robertglatzel@gmail.com>
2018-08-29 00:15:27 +0000

    fix extra $

--- arguments.c
+++ arguments.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv, char **env)
 	char *buffer = NULL, prev_cwd[200];
 	int rd = 0, ct = 0;
 	size_t size = 0;
-	int status = 1, i = 0, check;
+	int status = 1, i = 0, check, indicator = 0;
 	char *name;
 
 	(void)argc;
@@ -30,6 +30,8 @@ int main(int argc, char **argv, char **env)
 		check = 0;
 		write(STDOUT_FILENO, "$ ", 2);
 		rd = getline(&buffer, &size, stdin);
+		if (buffer[rd] != '\n')
+			indicator = 1;
 		for (i = 0; buffer[i] != '\0'; i++)
 			if (buffer[i] == ' ' || buffer[i] == '\t')
 				check++;
@@ -39,6 +41,8 @@ int main(int argc, char **argv, char **env)
 			status = 0;
 		else
 			exec_func(buffer, rd, name, prev_cwd, ct, env);
+		if (indicator == 1)
+			exit(0);
 	}
 	if (buffer)
 		free(buffer);
