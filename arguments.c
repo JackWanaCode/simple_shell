272f126291ee06996a1860a4532c5928227780a4
Robert <robertglatzel@gmail.com>
2018-08-29 01:24:51 +0000

    Put error pritner back in

--- arguments.c
+++ arguments.c
@@ -17,10 +17,9 @@ int main(int argc, char **argv, char **env)
 	int rd = 0, ct = 0;
 	size_t size = 0;
 	int status = 1, i = 0, check, indicator = 0;
-	char *name;
+	char *name = &(argv[0][2]);
 
 	(void)argc;
-	name = &(argv[0][2]);
 	_memset(prev_cwd, 0, 200);
 	_strcpy(prev_cwd, _getenv(env, "PWD"));
 	signal(SIGINT, sigintHandler);
@@ -30,9 +29,9 @@ int main(int argc, char **argv, char **env)
 		check = 0;
 		write(STDOUT_FILENO, "$ ", 2);
 		rd = getline(&buffer, &size, stdin);
-		if (buffer[rd] != '\n')
+/*		if (buffer[rd] != '\n')
 			indicator = 1;
-		for (i = 0; buffer[i] != '\0'; i++)
+*/		for (i = 0; buffer[i] != '\0'; i++)
 			if (buffer[i] == ' ' || buffer[i] == '\t')
 				check++;
 		if (rd == 1 || rd == check + 1)
@@ -41,9 +40,9 @@ int main(int argc, char **argv, char **env)
 			status = 0;
 		else
 			exec_func(buffer, rd, name, prev_cwd, ct, env);
-		if (indicator == 1)
+/*		if (indicator == 1)
 			exit(0);
-	}
+*/	}
 	if (buffer)
 		free(buffer);
 	write(1, "\n", 1);
